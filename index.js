9dd5cc52068e2eb6a9fa49d3b026970b76f88767
jitender <113350806+jitenderji1137@users.noreply.github.com>
2023-04-20 19:01:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // server.js
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 5001
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// server
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
